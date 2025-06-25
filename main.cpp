d23bb428b9679f1d45222cbcafb838890a1ccf61
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-06-25 20:19:20 +0400

    Add AddressSanitizer leak test workflow and project files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void leak_memory() {
+    int* ptr = new int[100];
+    ptr[0] = 42;
+    std::cout << "Leaked memory with value: " << ptr[0] << std::endl;
+}
+
+int main() {
+    leak_memory();
+    return 0;
+}
