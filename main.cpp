8f91dcb15fa71fcee63d94c24e8d5f81758c6e84
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-06-26 19:10:41 +0400

    Test the workflow without leaks

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 void leak_memory() {
-    int* ptr = new int[100];
-    ptr[0] = 42;
-    std::cout << "Leaked memory with value: " << ptr[0] << std::endl;
+    // int* ptr = new int[100];
+    // ptr[0] = 42;
+    // std::cout << "Leaked memory with value: " << ptr[0] << std::endl;
 }
 
 int main() {
